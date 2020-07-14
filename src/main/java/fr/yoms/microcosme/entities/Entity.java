8087332a3c8ecdf4401b99dda076ce2b5e250fef
Crooser <minecraftcrooserman@gmail.com>
2020-07-14 15:08:54 +0200

    Updated Entity system

--- src/main/java/fr/yoms/microcosme/entities/Entity.java
+++ src/main/java/fr/yoms/microcosme/entities/Entity.java
@@ -6,20 +6,17 @@ import java.awt.*;
 
 public abstract class Entity {
 
-    public static final Position DEFAULT_SPAWN_POSITION = new Position(0, 0);
-
     protected final int id;
-    protected Position position = DEFAULT_SPAWN_POSITION;
+    protected Position position;
+    protected int width;
+    protected int height;
 
-    public Entity(int id) {
-
-        this.id = id;
-    }
-
-    public Entity(int id, Position position) {
+    public Entity(int id, Position position, int width, int height) {
 
         this.id = id;
         this.position = position;
+        this.width = width;
+        this.height = height;
     }
 
     public int getId() {
@@ -31,6 +28,24 @@ public abstract class Entity {
     }
     public void setPosition(Position position) {
         this.position = position;
+    }
+
+    public int getWidth() {
+
+        return width;
+    }
+    public void setWidth(int width) {
+
+        this.width = width;
+    }
+
+    public int getHeight() {
+
+        return height;
+    }
+    public void setHeight(int height) {
+
+        this.height = height;
     }
 
     abstract void update();
--- src/main/java/fr/yoms/microcosme/entities/livings/LivingEntity.java
+++ src/main/java/fr/yoms/microcosme/entities/livings/LivingEntity.java
@@ -5,38 +5,13 @@ import fr.yoms.microcosme.utils.Position;
 
 public abstract class LivingEntity extends Entity {
 
-    public static final double DEFAULT_HEALTH = 20;
-    public static final int DEFAULT_AGE = 0;
+    protected double health;
+    protected int age;
 
-    protected double health = DEFAULT_HEALTH;
-    protected int age = DEFAULT_AGE;
+    public LivingEntity(int id, Position position, int width, int height, double health, int age) {
 
-    public LivingEntity(int id) {
-        super(id);
-    }
+        super(id, position, width, height);
 
-    public LivingEntity(int id, Position position) {
-        super(id, position);
-    }
-
-    public LivingEntity(int id, double health) {
-        super(id);
-        this.health = health;
-    }
-
-    public LivingEntity(int id, Position position, double health) {
-        super(id, position);
-        this.health = health;
-    }
-
-    public LivingEntity(int id, double health, int age) {
-        super(id);
-        this.health = health;
-        this.age = age;
-    }
-
-    public LivingEntity(int id, Position position, double health, int age) {
-        super(id, position);
         this.health = health;
         this.age = age;
     }
