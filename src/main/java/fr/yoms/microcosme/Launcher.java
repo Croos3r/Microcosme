2ee77a5810adc99765c893621905709dc6fe1824
Crooser <minecraftcrooserman@gmail.com>
2020-07-16 16:19:20 +0200

    Parameterized Launcher.java

--- src/main/java/fr/yoms/microcosme/Launcher.java
+++ src/main/java/fr/yoms/microcosme/Launcher.java
@@ -6,8 +6,20 @@ public class Launcher {
 
     public static void main(String[] args) {
 
-        Microcosme microcosme = new Microcosme(new Display("Microcosme", 1500, 1000), 60);
+        if (args.length >= 2 && args.length < 4) {
 
-        microcosme.start();
+            try {
+                int width = Integer.parseInt(args[0]);
+                int height = Integer.parseInt(args[1]);
+                int maxTPS = args.length == 3 ? Integer.parseInt(args[2]) : 60;
+
+                Microcosme microcosme = new Microcosme(new Display("Microcosme", width, height), maxTPS);
+
+                microcosme.start();
+            } catch (NumberFormatException ignored) {
+                System.err.println("Microcosme <width> <height> [maxTPS]");
+            }
+        }
+        else System.err.println("Microcosme <width> <height> [maxTPS]");
     }
 }
