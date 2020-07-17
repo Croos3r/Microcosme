aa2e0c718a4d7e9e268580445197d27932434082
Crooser <minecraftcrooserman@gmail.com>
2020-07-17 17:07:46 +0200

    Added multiple shape intersect checks

--- src/main/java/fr/yoms/microcosme/utils/Circle.java
+++ src/main/java/fr/yoms/microcosme/utils/Circle.java
@@ -1,13 +1,14 @@
 package fr.yoms.microcosme.utils;
 
 import java.awt.*;
+import java.awt.geom.Line2D;
 
 public class Circle {
 
     private Position center;
-    private int radius;
+    private double radius;
 
-    public Circle(Position center, int radius) {
+    public Circle(Position center, double radius) {
 
         this.center = center;
         this.radius = radius;
@@ -27,19 +28,19 @@ public class Circle {
 
         return Math.pow((position.getX() - center.getX()), 2) + Math.pow((position.getY() - center.getY()), 2) <= radius * radius;
     }
-    
-    public boolean intersect(Rectangle rectangle) {
 
-        double rectangleHalfWidth = (double) rectangle.width / 2;
-        double rectangleHalfHeight = (double) rectangle.width / 2;
-        
-        double cx = Math.abs(this.center.getX() - rectangle.x - rectangleHalfWidth);
+    public boolean intersectsRect(double x, double y, double width, double height) {
+
+        double rectangleHalfWidth = width / 2;
+        double rectangleHalfHeight = height / 2;
+
+        double cx = Math.abs(this.center.getX() - x - rectangleHalfWidth);
         double xDist = rectangleHalfWidth + this.radius;
-        
+
         if (cx > xDist)
             return false;
-        
-        double cy = Math.abs(this.center.getY() - rectangle.y - rectangleHalfHeight);
+
+        double cy = Math.abs(this.center.getY() - y - rectangleHalfHeight);
         double yDist = rectangleHalfHeight + this.radius;
         if (cy > yDist)
             return false;
@@ -52,11 +53,49 @@ public class Circle {
         double maxCornerDistSq = this.radius * this.radius;
         return xCornerDistSq + yCornerDistSq <= maxCornerDistSq;
     }
-    public int getRadius() {
+    public boolean intersectsRect(Position origin, Dimension size) {
+
+        return intersectsRect(origin.getX(), origin.getY(), size.getWidth(), size.getHeight());
+    }
+    public boolean intersectsRect(Rectangle rectangle) {
+
+        return intersectsRect(rectangle.x, rectangle.y, rectangle.getWidth(), rectangle.getHeight());
+    }
+    public boolean intersectsLine(double aX, double aY, double bX, double bY) {
+
+        double Dr = Math.hypot(bX - aX, bY - aY);
+        double D = aX * bY - bX * aY;
+
+        double delta = (radius * radius) * (Dr * Dr) - (D * D);
+
+        return delta >= 0;
+    }
+    public boolean intersectsLine(Position origin, Position end) {
+
+        return intersectsLine(origin.getX(), origin.getY(), end.getX(), end.getY());
+    }
+    public boolean intersectsLine(Line2D line) {
+
+        return intersectsLine(line.getX1(), line.getY1(), line.getX2(), line.getY2());
+    }
+    public boolean intersectsCircle(double cX, double cY, double radius) {
+
+        return Math.hypot(center.getX() - cX, center.getY() - cY) <= (this.radius + radius);
+    }
+    public boolean intersectsCircle(Position center, double radius) {
+
+        return intersectsCircle(center.getX(), center.getY(), radius);
+    }
+    public boolean intersectsCircle(Circle circle) {
+
+        return intersectsCircle(circle.center.getX(), circle.center.getY(), circle.radius);
+    }
+
+    public double getRadius() {
 
         return radius;
     }
-    public void setRadius(int radius) {
+    public void setRadius(double radius) {
 
         this.radius = radius;
     }
